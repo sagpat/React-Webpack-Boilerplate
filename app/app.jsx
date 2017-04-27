012495ebc3cf5cba08d7d4e8883c37bb18e7a5a3
Sagar Patil <saagar.1647@gmail.com>
2017-04-27 16:14:31 +0530

    added ES6 helper files

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ObjOne = {
+  name: 'Sagar',
+  location: 'Pune'
+};
+
+var ObjTwo = {
+  age: 25,
+  ...ObjOne
+};
+
+console.log(ObjTwo);
+
 ReactDOM.render(
   <h1>Boilerplate App</h1>,
   document.getElementById('app')
